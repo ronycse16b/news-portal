71756f3c6351c09a901c1d4a5798be9c8967a747
ronycse16b <rony.cse16b@gmail.com>
2022-09-04 14:03:15 +0600

    Modal Conmplite

--- js/function.js
+++ js/function.js
@@ -5,7 +5,7 @@ const loadNewses = async(catagory) =>{
     const data = await res.json();
 
      displayNewsCatagory(data.data);
-     console.log(data.data);
+
 
  }
 
@@ -21,7 +21,8 @@ const displayNewsCatagory = newses =>{
 
         catagoryDiv.innerHTML = `
      
-    <div class="card mt-4 w-100 " data-bs-toggle="modal" data-bs-target="#showDetailsNews">
+    <div class="card mt-4 w-100 " data-bs-toggle="modal" data-bs-target="#showDetailsNews " onclick="newsDetailsShow('${news._id}');" >
+   
         <div class ="row g-0 ">
           <div class="col-md-4">
             <img src="${news.image_url}" class="img-fluid rounded-start" alt="...">
@@ -41,8 +42,7 @@ const displayNewsCatagory = newses =>{
             </div>
 
             <i class="fa-solid fa-eye "></i><span>${news.total_view}</span>
-          
-  
+         
             <i class="ms-5 fs-3  fa-solid fa-arrow-right"></i>
             </div>
           
@@ -59,8 +59,38 @@ const displayNewsCatagory = newses =>{
 
 
 
+const newsDetailsShow = async(news_id) =>{
+  const url =`https://openapi.programming-hero.com/api/news/${news_id}`
+  const res =await fetch(url);
+  const data = await res.json();
 
 
+  newsDisplay(data.data[0]);
+
+
+}
+
+const newsDisplay = newsid =>{
+
+ const modalDetails = document.getElementById("showDetailsNewsLabel");
+    modalDetails.innerText= newsid.title;
+ const detailsImage= document.getElementById("details_imge");
+   detailsImage.src =newsid.thumbnail_url;
+
+    const detailsNews =document.getElementById("detailsnews");
+    detailsNews.innerText = newsid.details;
+
+    const newsAuthor = document.getElementById("imgeAutor");
+     newsAuthor.innerText ="News Publish by :"+ newsid.author.name;
+     
+
+    const newsPublishDate = document.getElementById("publish-date");
+    newsPublishDate.innerText = newsid.author.published_date;
+    const detailsPublisherImage= document.getElementById("detailsPublisher_imge");
+   detailsPublisherImage.src =newsid.author.img
+
+console.log(newsid);
+}
 
 
 // document.getElementById("btn-search").addEventListener("click",function(){
